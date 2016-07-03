2db21ff524376adcdf7e6ef0bc397a2d3d66b260
Dorian Amouroux <dor.amouroux@gmail.com>
2016-07-03 15:52:34 +0200

    Adapt exists for collect static --clear
    When you call `collectstatic --clear`, the function will call the function `clear_dir` with an empty string: https://github.com/django/django/blob/master/django/contrib/staticfiles/management/commands/collectstatic.py#L223
    
    `clear_dir` will call exists with the parameters it receives, and stop if exists return `False`. 
    I add a check to the name length and return True if it's an empty string to be sure it goes through the whole --clear process

--- azure_storage/storage.py
+++ azure_storage/storage.py
@@ -141,6 +141,8 @@ class AzureStorage(Storage):
         Returns True if a file referenced by the given name already exists in
         the storage system, or False if the name is available for a new file.
         """
+        if (len(name) == 0):
+            return True
         try:
             self._get_properties(name)
 
